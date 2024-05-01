9d3966bfbb3ec776ea7238bdbee15d9a4e2f2fe3
Владислав Ильин <vladi5lav.ilin@yandex.ru>
2024-05-01 22:37:25 +0300

    Добавьте файлы проекта.

---
+++ WPF23_1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF23_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = Login.Text;
+            string password = Password.Text;
+
+            if (username == "Simple" && password == "Simple")
+            {
+                Simple simpleWindow = new Simple();
+                simpleWindow.Show();
+            }
+            else if (username == "Premium" && password == "Premium")
+            {
+                Premium premiumWindow = new Premium();
+                premiumWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль");
+            }
+        }
+    }
+}
